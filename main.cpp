8da18a99928a3ce85002c8e8bd88fce36d24e0c5
ralfcheung <ralfcheung@wireless-plus-015-003.usc.edu>
2014-06-26 03:47:20 -0700

    deleted unnecessary code

--- main.cpp
+++ main.cpp
@@ -77,8 +77,7 @@ int main( int argc, char** argv )
     printf("%s", argv[1]);
     
     
-//    cv::Mat object = cv::imread( argv[1], CV_LOAD_IMAGE_COLOR );
-    FILE *f = fopen(argv[1], "rb");
+   FILE *f = fopen(argv[1], "rb");
     if (!f) {
         printf("error\n");
         exit(1);
@@ -103,7 +102,6 @@ int main( int argc, char** argv )
     cv::Rect myROI(100, 100, 150, 150);
     
     
-    //Detect the keypoints using SURF Detector
     int minHessian = 1000;
     
     cv::SurfFeatureDetector detector( minHessian );
@@ -111,7 +109,6 @@ int main( int argc, char** argv )
     detector.upright = false;
     detector.detect( object, kp_object );
     
-    //Calculate descriptors (feature vectors)
     cv::SurfDescriptorExtractor extractor;
     cv::Mat des_object;
     
@@ -134,9 +131,7 @@ int main( int argc, char** argv )
     int framecount = 0;
     while (key != 27)
     {
-        
-        
-        
+       
         
         if (framecount < 5)
         {
@@ -152,7 +147,7 @@ int main( int argc, char** argv )
         std::vector<cv::Point2f> scene;
         std::vector<cv::Point2f> scene_corners(4);
         cv::Mat H;
-//        cv::Mat image = cv::imread( argv[2], CV_LOAD_IMAGE_COLOR );
+
         MyImage *sceneMyPic = new MyImage();
         sceneMyPic->setWidth(352);
         sceneMyPic->setHeight(288);
@@ -175,7 +170,6 @@ int main( int argc, char** argv )
             }
         }
         
-        //Draw only "good" matches
         drawMatches( object, kp_object, image, kp_image, good_matches, img_matches, cv::Scalar::all(-1), cv::Scalar::all(-1), std::vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
         
         if (good_matches.size() >= 4)
@@ -184,7 +178,6 @@ int main( int argc, char** argv )
             Point2f averagePoint;
             for( int i = 0; i < good_matches.size(); i++ )
             {
-                //Get the keypoints from the good matches
                 obj.push_back( kp_object[ good_matches[i].queryIdx ].pt );
                 scene.push_back( kp_image[ good_matches[i].trainIdx ].pt );
                 averagePoint.x += kp_image[ good_matches[i].trainIdx ].pt.x;
@@ -235,7 +228,6 @@ int main( int argc, char** argv )
                         
                         const float* ranges[] = { h_ranges, s_ranges };
                         
-                        // Use the o-th and 1-st channels
                         int channels[] = { 0, 1 };
                         MatND hist_base;
                         MatND hist_test;
@@ -263,9 +255,6 @@ int main( int argc, char** argv )
             }
             
             
-//            H = findHomography( obj, scene, CV_RANSAC );
-//            
-//            perspectiveTransform( obj_corners, scene_corners, H);
 
             
             line(img_matches,  cv::Point2f(averagePoint.x - 40 + sceneMyPic->getWidth(), averagePoint.y - 40), cv::Point2f(averagePoint.x + 40 + sceneMyPic->getWidth(), averagePoint.y - 40), cv::Scalar(0, 255, 0), 4);
@@ -282,134 +271,3 @@ int main( int argc, char** argv )
     return 0;
 
 }
-
-
-IplImage* convertImageRGBtoHSV(const IplImage *imageRGB)
-{
-	float fR, fG, fB;
-	float fH, fS, fV;
-	const float FLOAT_TO_BYTE = 255.0f;
-	const float BYTE_TO_FLOAT = 1.0f / FLOAT_TO_BYTE;
-    
-	// Create a blank HSV image
-	IplImage *imageHSV = cvCreateImage(cvGetSize(imageRGB), 8, 3);
-	if (!imageHSV || imageRGB->depth != 8 || imageRGB->nChannels != 3) {
-		printf("ERROR in convertImageRGBtoHSV()! Bad input image.\n");
-		exit(1);
-	}
-    
-	int h = imageRGB->height;		// Pixel height.
-	int w = imageRGB->width;		// Pixel width.
-	int rowSizeRGB = imageRGB->widthStep;	// Size of row in bytes, including extra padding.
-	char *imRGB = imageRGB->imageData;	// Pointer to the start of the image pixels.
-	int rowSizeHSV = imageHSV->widthStep;	// Size of row in bytes, including extra padding.
-	char *imHSV = imageHSV->imageData;	// Pointer to the start of the image pixels.
-	for (int y=0; y<h; y++) {
-		for (int x=0; x<w; x++) {
-			// Get the RGB pixel components. NOTE that OpenCV stores RGB pixels in B,G,R order.
-			uchar *pRGB = (uchar*)(imRGB + y*rowSizeRGB + x*3);
-			int bB = *(uchar*)(pRGB+0);	// Blue component
-			int bG = *(uchar*)(pRGB+1);	// Green component
-			int bR = *(uchar*)(pRGB+2);	// Red component
-            
-			// Convert from 8-bit integers to floats.
-			fR = bR * BYTE_TO_FLOAT;
-			fG = bG * BYTE_TO_FLOAT;
-			fB = bB * BYTE_TO_FLOAT;
-            
-			// Convert from RGB to HSV, using float ranges 0.0 to 1.0.
-			float fDelta;
-			float fMin, fMax;
-			int iMax;
-			// Get the min and max, but use integer comparisons for slight speedup.
-			if (bB < bG) {
-				if (bB < bR) {
-					fMin = fB;
-					if (bR > bG) {
-						iMax = bR;
-						fMax = fR;
-					}
-					else {
-						iMax = bG;
-						fMax = fG;
-					}
-				}
-				else {
-					fMin = fR;
-					fMax = fG;
-					iMax = bG;
-				}
-			}
-			else {
-				if (bG < bR) {
-					fMin = fG;
-					if (bB > bR) {
-						fMax = fB;
-						iMax = bB;
-					}
-					else {
-						fMax = fR;
-						iMax = bR;
-					}
-				}
-				else {
-					fMin = fR;
-					fMax = fB;
-					iMax = bB;
-				}
-			}
-			fDelta = fMax - fMin;
-			fV = fMax;				// Value (Brightness).
-			if (iMax != 0) {			// Make sure it's not pure black.
-				fS = fDelta / fMax;		// Saturation.
-				float ANGLE_TO_UNIT = 1.0f / (6.0f * fDelta);	// Make the Hues between 0.0 to 1.0 instead of 6.0
-				if (iMax == bR) {		// between yellow and magenta.
-					fH = (fG - fB) * ANGLE_TO_UNIT;
-				}
-				else if (iMax == bG) {		// between cyan and yellow.
-					fH = (2.0f/6.0f) + ( fB - fR ) * ANGLE_TO_UNIT;
-				}
-				else {				// between magenta and cyan.
-					fH = (4.0f/6.0f) + ( fR - fG ) * ANGLE_TO_UNIT;
-				}
-				// Wrap outlier Hues around the circle.
-				if (fH < 0.0f)
-					fH += 1.0f;
-				if (fH >= 1.0f)
-					fH -= 1.0f;
-			}
-			else {
-				// color is pure Black.
-				fS = 0;
-				fH = 0;	// undefined hue
-			}
-            
-			// Convert from floats to 8-bit integers.
-			int bH = (int)(0.5f + fH * 255.0f);
-			int bS = (int)(0.5f + fS * 255.0f);
-			int bV = (int)(0.5f + fV * 255.0f);
-            
-			// Clip the values to make sure it fits within the 8bits.
-			if (bH > 255)
-				bH = 255;
-			if (bH < 0)
-				bH = 0;
-			if (bS > 255)
-				bS = 255;
-			if (bS < 0)
-				bS = 0;
-			if (bV > 255)
-				bV = 255;
-			if (bV < 0)
-				bV = 0;
-            
-			// Set the HSV pixel components.
-			uchar *pHSV = (uchar*)(imHSV + y*rowSizeHSV + x*3);
-			*(pHSV+0) = bH;		// H component
-			*(pHSV+1) = bS;		// S component
-			*(pHSV+2) = bV;		// V component
-		}
-	}
-	return imageHSV;
-}
-
