b34680f016d903f14b6c00d5f6eb5b3946eca7fb
ralfcheung <ralfcheung@wireless-plus-015-003.usc.edu>
2014-06-26 03:43:17 -0700

    edited delimiters

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  OpenCV Test
+//  Reverse image search
 //
 //  Created by Ralf Cheung on 4/12/14.
 //  Copyright (c) 2014 Ralf Cheung. All rights reserved.
@@ -21,22 +21,62 @@ void readme();
 IplImage* convertImageRGBtoHSV(const IplImage *imageRGB);
 
 
+std::vector<string> saveFileName(char *fileName){
+    std::string s = fileName;
+    std::string delimiter = "_., ";
+    
+    size_t pos = 0;
+    std::string token;
+    std::vector<string> sourceVector;
+    
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+        token = s.substr(0, pos);
+        sourceVector.push_back(token);
+        s.erase(0, pos + delimiter.length());
+    }
+    return sourceVector;
+}
+
 /** @function main */
 int main( int argc, char** argv )
 {
     //300 350
     
-    //    convertImageRGBtoHSV()
+    double probability = 0.6;
+
+    std::vector<string> sourceVector = saveFileName(argv[1]);
+    std::vector<string> searchVector = saveFileName(argv[2]);
+
+    
+    double matched = 0;
+    
+    for(std::vector<string>::iterator sourceIterator = sourceVector.begin(); sourceIterator != sourceVector.end(); sourceIterator++ ){
+        for(std::vector<string>::iterator searchIterator = searchVector.begin(); searchIterator != searchVector.end(); searchIterator++ ){
+            if (*sourceIterator == *searchIterator) {
+                matched++;
+                break;
+            }
+            
+            
+        }
+    }
+
+    probability *= (matched / sourceVector.size());
+    
+    
     
     MyImage *image = new MyImage();
     image->setWidth(352);
     image->setHeight(288);
     image->setImagePath(argv[1]);
-    std::cout << argv[1];
+
     if(!image->ReadImage()){
         std::cout << "Error reading MyImage" << std::endl;
     }
 
+    printf("%s", argv[1]);
+    
+    
 //    cv::Mat object = cv::imread( argv[1], CV_LOAD_IMAGE_COLOR );
     FILE *f = fopen(argv[1], "rb");
     if (!f) {
@@ -47,10 +87,9 @@ int main( int argc, char** argv )
     fread(pixels, sizeof(unsigned char), 352*288 * 3, f);
     fclose(f);
     cv::Mat object(Size(352, 288), CV_8UC3, pixels);
-//    cv::cvtColor(object, object, CV_RGB2GRAY);
     
-//    namedWindow("image", CV_WINDOW_AUTOSIZE);
-//    imshow("image", object);
+    namedWindow("image", CV_WINDOW_AUTOSIZE);
+    imshow("image", object);
     
     if( !object.data )
     {
@@ -60,26 +99,16 @@ int main( int argc, char** argv )
     
     cv::Mat tmp, alpha;
     threshold(object, object, 255, 0, THRESH_TOZERO_INV);
-//    Size dynSize(0,0);
-//    dynSize.width = object.cols * 0.6;
-//    dynSize.height = object.rows * 0.6;
-//    int dynType = CV_8UC1;
-//
-//    Mat dynMat(dynSize, dynType);
-//    
-//    resize(object, dynMat, CV_INTER_LINEAR);
+
     cv::Rect myROI(100, 100, 150, 150);
     
-//    cv::Mat cropped = object(myROI);
-//    object = object(myROI);
-//    imshow("blah", object);
     
     //Detect the keypoints using SURF Detector
-    int minHessian = 20;
+    int minHessian = 1000;
     
     cv::SurfFeatureDetector detector( minHessian );
     std::vector<cv::KeyPoint> kp_object;
-    
+    detector.upright = false;
     detector.detect( object, kp_object );
     
     //Calculate descriptors (feature vectors)
@@ -105,7 +134,9 @@ int main( int argc, char** argv )
     int framecount = 0;
     while (key != 27)
     {
-        cv::Mat frame = cv::imread( argv[2], CV_LOAD_IMAGE_COLOR );
+        
+        
+        
         
         if (framecount < 5)
         {
@@ -121,10 +152,15 @@ int main( int argc, char** argv )
         std::vector<cv::Point2f> scene;
         std::vector<cv::Point2f> scene_corners(4);
         cv::Mat H;
-        cv::Mat image = cv::imread( argv[2], CV_LOAD_IMAGE_COLOR );
+//        cv::Mat image = cv::imread( argv[2], CV_LOAD_IMAGE_COLOR );
+        MyImage *sceneMyPic = new MyImage();
+        sceneMyPic->setWidth(352);
+        sceneMyPic->setHeight(288);
+        sceneMyPic->setImagePath(argv[2]);
+        sceneMyPic->ReadImage();
 
-        
-//        cvtColor(frame, image, CV_RGB2GRAY);
+        cv::Mat image(Size(352, 288), CV_8UC3, sceneMyPic->getImageData());
+
         
         detector.detect( image, kp_image );
         extractor.compute( image, kp_image, des_image );
@@ -167,29 +203,79 @@ int main( int argc, char** argv )
                     inRange++;
                 }
                 
-            }
+            } 
 
-            if ((float)inRange / good_matches.size() > 0.8) {
-                printf("\nfound\n");
+            if (probability + (double)inRange / good_matches.size() > 0.8) {
+                printf("found\n");
             }else{
-                printf("\nnot found\n");
+                MyImage *objectPic = new MyImage();
+                objectPic->setWidth(352);
+                objectPic->setHeight(288);
+                objectPic->setImagePath(argv[1]);
+                objectPic->ReadImage();
+                
+                cv::Mat objectImage(Size(352, 288), CV_8UC3, objectPic->getImageData());
+                cv::Mat smallerQueryImage;
+                resize(objectImage, smallerQueryImage, Size(16, 16), 0,0, INTER_CUBIC);
+
+                cvtColor(objectImage, objectImage, COLOR_RGB2HSV);
+
+                for (int x = 0; x < 352; x += 16) {
+                    for (int y = 0; y < 288; y += 16) {
+                        Rect region(Point(x, y), Size(16, 16));
+                        
+                        cv::Mat subSampleOfScene = image(region);
+
+                        cvtColor(subSampleOfScene, subSampleOfScene, COLOR_RGB2HSV);
+                        int h_bins = 50; int s_bins = 60;
+                        int histSize[] = { h_bins, s_bins };
+
+                        float h_ranges[] = { 0, 180 };
+                        float s_ranges[] = { 0, 256 };
+                        
+                        const float* ranges[] = { h_ranges, s_ranges };
+                        
+                        // Use the o-th and 1-st channels
+                        int channels[] = { 0, 1 };
+                        MatND hist_base;
+                        MatND hist_test;
+                        calcHist( &smallerQueryImage, 1, channels, Mat(), hist_base, 2, histSize, ranges, true, false );
+                        normalize( hist_base, hist_base, 0, 1, NORM_MINMAX, -1, Mat() );
+
+                        calcHist( &subSampleOfScene, 1, channels, Mat(), hist_test, 2, histSize, ranges, true, false );
+                        normalize( hist_test, hist_test, 0, 1, NORM_MINMAX, -1, Mat() );
+                        double base_test1 = compareHist(hist_base, hist_test, CV_COMP_CORREL);
+                        if (base_test1 > 0.1) {
+                            probability += base_test1;
+                        }
+
+                    }
+                }
+                
+                if (probability > 0.8) { 
+                    
+                    printf("found with confidence: %f\n", probability);
+                }
+                else{
+                    printf("not found\n");
+                }
+               
             }
             
             
-            H = findHomography( obj, scene, CV_RANSAC );
-            
-            perspectiveTransform( obj_corners, scene_corners, H);
+//            H = findHomography( obj, scene, CV_RANSAC );
+//            
+//            perspectiveTransform( obj_corners, scene_corners, H);
 
             
-            //Draw lines between the corners (the mapped object in the scene image )
-            line( img_matches, scene_corners[0] + cv::Point2f( object.cols, 0), scene_corners[1] + cv::Point2f( object.cols, 0), cv::Scalar(0, 255, 0), 4 );
-            line( img_matches, scene_corners[1] + cv::Point2f( object.cols, 0), scene_corners[2] + cv::Point2f( object.cols, 0), cv::Scalar( 0, 255, 0), 4 );
-            line( img_matches, scene_corners[2] + cv::Point2f( object.cols, 0), scene_corners[3] + cv::Point2f( object.cols, 0), cv::Scalar( 0, 255, 0), 4 );
-            line( img_matches, scene_corners[3] + cv::Point2f( object.cols, 0), scene_corners[0] + cv::Point2f( object.cols, 0), cv::Scalar( 0, 255, 0), 4 );
+            line(img_matches,  cv::Point2f(averagePoint.x - 40 + sceneMyPic->getWidth(), averagePoint.y - 40), cv::Point2f(averagePoint.x + 40 + sceneMyPic->getWidth(), averagePoint.y - 40), cv::Scalar(0, 255, 0), 4);
+            line(img_matches, cv::Point2f(averagePoint.x + 40 + sceneMyPic->getWidth(), averagePoint.y - 40), cv::Point2f(averagePoint.x + 40 + sceneMyPic->getWidth(), averagePoint.y + 40), cv::Scalar(0, 255, 0), 4);
+            line(img_matches, cv::Point2f(averagePoint.x + 40 + sceneMyPic->getWidth(), averagePoint.y + 40), cv::Point2f(averagePoint.x - 40 + sceneMyPic->getWidth(), averagePoint.y + 40), cv::Scalar(0, 255, 0), 4);
+            line(img_matches, cv::Point2f(averagePoint.x - 40 + sceneMyPic->getWidth(), averagePoint.y + 40), cv::Point2f(averagePoint.x - 40 + sceneMyPic->getWidth(), averagePoint.y - 40), cv::Scalar(0, 255, 0), 4);
+
+            
         }
-        
-        //Show detected matches
-        imshow( "Good Matches", img_matches );
+        imshow("Good matches", img_matches);
         
         key = cv::waitKey(0);
     }
